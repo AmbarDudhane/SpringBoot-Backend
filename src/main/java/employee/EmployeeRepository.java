b622f1c75f40b37c06742231d8aaab184b6856bc
AmbarDudhane <ambarsd12345@hotmail.com>
2020-01-08 03:03:10 -0600

    Update EmployeeRepository.java

--- src/main/java/employee/EmployeeRepository.java
+++ src/main/java/employee/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package employee;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -7,4 +9,6 @@ public interface EmployeeRepository extends CrudRepository<Employee, Long>{
 
 	@Query("SELECT max(e.id) from Employee e")
 	public Long findMaxId();
+	
+	public Optional<Employee> findById(Long id);
 }
